e11f90061b123cd93d0c849900f89b4d04284586
1wasilah1 <m.wasilahhadi@gmail.com>
2021-08-26 05:16:09 +0700

    Setelah install Axios

---
+++ src/Repo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import axios from 'axios';
+
+export default class PersonList extends React.Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/1wasilah1/repos`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        { this.state.persons.map(person => <li>{person.name}</li>)}
+      </ul>
+    )
+  }
+}
