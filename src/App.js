9096f4b9bf56da300b2a81db72766e10305bed56
1wasilah1 <m.wasilahhadi@gmail.com>
2021-08-26 07:03:29 +0700

    penambahan fungsi calling github

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import axios from 'axios';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 export default class PersonList extends React.Component {
-  state = {
-    ListRepo: []
+  
+  constructor(props){
+	  super(props);
+	  this.state = {
+		ListRepo: []
+	  }
+	  this.handleSubmit.bind(this);
   }
+  
 	async handleSubmit(e) {
            e.preventDefault();
-		   alert("tes");
+		   console.log(e.target.githubUser.value);
            
     }
-  componentDidMount() {
+  callingGithub() {
     axios.get(`https://api.github.com/users/1wasilah1/repos`)
       .then(res => {
         const ListRepo = res.data;
@@ -27,7 +33,7 @@ export default class PersonList extends React.Component {
 
                 <div className="form-group">
               
-                    <input type="text" className="form-control" placeholder="Enter Username" />
+                    <input type="text" name="githubUser" className="form-control" placeholder="Enter Username" />
                 </div>
 
                 <button type="submit" className="btn btn-primary btn-block">Cari</button>
